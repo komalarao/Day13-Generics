43b276f4ec90e52943572bf6a9e021b36bf6d599
komalarao <manoharvakamullu1999@gmail.com>
2023-01-21 01:07:25 -0800

    added: program to find max int among three numbers

---
+++ src/com/bridgelabz/generics/MaximumInteger.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.generics;
+
+public class MaximumInteger {
+	
+	public static Integer maximumInteger(Integer num1, Integer num2, Integer num3) {
+
+		Integer maximumNumber = num1;
+		if (num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0)
+			maximumNumber = num1;
+		else if (num2.compareTo(num1) > 0 && num2.compareTo(num3) > 0)
+			maximumNumber = num2;
+		else
+			maximumNumber = num3;
+		return maximumNumber;
+	}
+	// print maximum number
+
+	public static void main(String[] args) {
+		System.out.println("Maximum Number among 3 will be: " + maximumInteger(70, 40, 50));
+	}
+}
